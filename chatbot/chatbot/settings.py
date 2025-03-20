edbaccb63e176c355656f3a6ff7af7e9250932bf
Glison-Pereira <145424111+Glison-Pereira@users.noreply.github.com>
2025-03-20 15:51:22 +0530

    Added gunicorn, whitenoise, and cleaned dependencies

--- chatbot/chatbot/settings.py
+++ chatbot/chatbot/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 STATIC_ROOT= BASE_DIR / 'Staticproduction'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']  # Tell Django to look in the "static" folder
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
